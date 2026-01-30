954f710b46e706b82f3dce5b921542a9331afc31
chiidu10:23 <r124214@g.kobe-kosen.ac.jp>
2026-01-31 02:55:01 +0900

    .cppも直さないといけねーじゃん
    .cppも直さないといけねーじゃん

--- src/RoboMaslib.cpp
+++ src/RoboMaslib.cpp
@@ -1,32 +1,99 @@
 #include "RoboMaslib.h"
 
+// セットアップ
+void RoboMaslib::setup(void) {
+    val.setup(); //
+}
+
+// 指令の送信
 void RoboMaslib::set(int id_) {
-    val.set(id_);
+    val.set(id_); //
 }
 
-void RoboMaslib::update(int id, int mode, int send) {
-    // val.updateの引数に合わせてrpm_には0を渡しています
-    val.update(id, send, 0);
+// 指令値の更新
+void RoboMaslib::update(int id, int send) {
+    val.update(id, send, send); //
 }
 
-void RoboMaslib::info(int _id) {
-    val.info(_id);
+// データの取得と累積エンコーダの計算
+void RoboMaslib::info(uint8_t _id, int P) {
+    val.info(_id); //
     
-    int16_t current_rpm = 0;
-    switch(_id) {
-        case 1: rpm1 = val.speed1; current_rpm = rpm1; break;
-        case 2: rpm2 = val.speed2; current_rpm = rpm2; break;
-        case 3: rpm3 = val.speed3; current_rpm = rpm3; break;
-        case 4: rpm4 = val.speed4; current_rpm = rpm4; break;
-        case 5: rpm5 = val.speed5; current_rpm = rpm5; break;
-        case 6: rpm6 = val.speed6; current_rpm = rpm6; break;
-        case 7: rpm7 = val.speed7; current_rpm = rpm7; break;
-        case 8: rpm8 = val.speed8; current_rpm = rpm8; break;
-        default: return;
-    }
+    // P=19の場合の補正係数
+    float ratio = (P == 19) ? 19.2 : (float)P; //
 
-    Serial.print("can");
-    Serial.print(_id);
-    Serial.print(": ");
-    Serial.println(current_rpm);
+    switch(_id) {
+        case 1:
+            rpm1 = val.speed1; //
+            encoder1 = val.encoder1 / ratio; //
+            if(encoder1 == 8191) e1_pre_pre = 1; else e1_pre_pre = 0; //
+            if(e1_pre_pre == 1 && e1_pre == 1) encoder1 -= 8191; //
+            if(encoder1 == 0) e1_pre = 1; else e1_pre = 0; //
+            if(e1_pre == 1 && e1 == 1) encoder1 += 8191; //
+            if(encoder1 == 8191) e1 = 1; else e1 = 0; //
+            break;
+        case 2:
+            rpm2 = val.speed2; //
+            encoder2 = val.encoder2 / ratio; //
+            if(encoder2 == 8191) e2_pre_pre = 1; else e2_pre_pre = 0; //
+            if(e2_pre_pre == 1 && e2_pre == 1) encoder2 -= 8191; //
+            if(encoder2 == 0) e2_pre = 1; else e2_pre = 0; //
+            if(e2_pre == 1 && e2 == 1) encoder2 += 8191; //
+            if(encoder2 == 8191) e2 = 1; else e2 = 0; //
+            break;
+        case 3:
+            rpm3 = val.speed3; //
+            encoder3 = val.encoder3 / ratio; //
+            if(encoder3 == 8191) e3_pre_pre = 1; else e3_pre_pre = 0; //
+            if(e3_pre_pre == 1 && e3_pre == 1) encoder3 -= 8191; //
+            if(encoder3 == 0) e3_pre = 1; else e3_pre = 0; //
+            if(e3_pre == 1 && e3 == 1) encoder3 += 8191; //
+            if(encoder3 == 8191) e3 = 1; else e3 = 0; //
+            break;
+        case 4:
+            rpm4 = val.speed4; //
+            encoder4 = val.encoder4 / ratio; //
+            if(encoder4 == 8191) e4_pre_pre = 1; else e4_pre_pre = 0; //
+            if(e4_pre_pre == 1 && e4_pre == 1) encoder4 -= 8191; //
+            if(encoder4 == 0) e4_pre = 1; else e4_pre = 0; //
+            if(e4_pre == 1 && e4 == 1) encoder4 += 8191; //
+            if(encoder4 == 8191) e4 = 1; else e4 = 0; //
+            break;
+        case 5:
+            rpm5 = val.speed5; //
+            encoder5 = val.encoder5 / ratio; //
+            if(encoder5 == 8191) e5_pre_pre = 1; else e5_pre_pre = 0; //
+            if(e5_pre_pre == 1 && e5_pre == 1) encoder5 -= 8191; //
+            if(encoder5 == 0) e5_pre = 1; else e5_pre = 0; //
+            if(e5_pre == 1 && e5 == 1) encoder5 += 8191; //
+            if(encoder5 == 8191) e5 = 1; else e5 = 0; //
+            break;
+        case 6:
+            rpm6 = val.speed6; //
+            encoder6 = val.encoder6 / ratio; //
+            if(encoder6 == 8191) e6_pre_pre = 1; else e6_pre_pre = 0; //
+            if(e6_pre_pre == 1 && e6_pre == 1) encoder6 -= 8191; //
+            if(encoder6 == 0) e6_pre = 1; else e6_pre = 0; //
+            if(e6_pre == 1 && e6 == 1) encoder6 += 8191; //
+            if(encoder6 == 8191) e6 = 1; else e6 = 0; //
+            break;
+        case 7:
+            rpm7 = val.speed7; //
+            encoder7 = val.encoder7 / ratio; //
+            if(encoder7 == 8191) e7_pre_pre = 1; else e7_pre_pre = 0; //
+            if(e7_pre_pre == 1 && e7_pre == 1) encoder7 -= 8191; //
+            if(encoder7 == 0) e7_pre = 1; else e7_pre = 0; //
+            if(e7_pre == 1 && e7 == 1) encoder7 += 8191; //
+            if(encoder7 == 8191) e7 = 1; else e7 = 0; //
+            break;
+        case 8:
+            rpm8 = val.speed8; //
+            encoder8 = val.encoder8 / ratio; //
+            if(encoder8 == 8191) e8_pre_pre = 1; else e8_pre_pre = 0; //
+            if(e8_pre_pre == 1 && e8_pre == 1) encoder8 -= 8191; //
+            if(encoder8 == 0) e8_pre = 1; else e8_pre = 0; //
+            if(e8_pre == 1 && e8 == 1) encoder8 += 8191; //
+            if(encoder8 == 8191) e8 = 1; else e8 = 0; //
+            break;
+    }
 }
--- src/RoboMasval.cpp
+++ src/RoboMasval.cpp
@@ -1,106 +1,80 @@
 #include "RoboMasval.h"
 
 void RoboMasval::setup(void) {
-    Serial.println(F("Hello Teensy 4.1 CAN Test."));
-    can3.begin();
-    can3.setBaudRate(1000000);
+    Serial.println(F("Hello Teensy 4.1 CAN Test.")); //
+    can3.begin(); //
+    can3.setBaudRate(1000000); //
 }
 
 void RoboMasval::set(int id_) {
-    // 送信頻度の制限（3ms以下ならスキップ）
-    if (millis() - pre_time_ < 3) return;
-    pre_time_ = millis();
+    if (millis() - pre_time_ < 3) return; //
+    pre_time_ = millis(); //
 
-    // ID 0x200 への送信設定 (Motor 1-4)
-    msg_send.id = 0x200;
-    msg_send.len = 8;
+    // モーター 1-4 用 (ID 0x200)
+    msg_send.id = 0x200; //
+    msg_send.len = 8; //
     switch (id_) {
-        case 1:
-            msg_send.buf[0] = (motor1 >> 8) & 0xFF;
-            msg_send.buf[1] = motor1 & 0xFF;
-            break;
-        case 2:
-            msg_send.buf[2] = (motor2 >> 8) & 0xFF;
-            msg_send.buf[3] = motor2 & 0xFF;
-            break;
-        case 3:
-            msg_send.buf[4] = (motor3 >> 8) & 0xFF;
-            msg_send.buf[5] = motor3 & 0xFF;
-            break;
-        case 4:
-            msg_send.buf[6] = (motor4 >> 8) & 0xFF;
-            msg_send.buf[7] = motor4 & 0xFF;
-            break;
+        case 1: msg_send.buf[0] = (motor1 >> 8) & 0xFF; msg_send.buf[1] = motor1 & 0xFF; break; //
+        case 2: msg_send.buf[2] = (motor2 >> 8) & 0xFF; msg_send.buf[3] = motor2 & 0xFF; break; //
+        case 3: msg_send.buf[4] = (motor3 >> 8) & 0xFF; msg_send.buf[5] = motor3 & 0xFF; break; //
+        case 4: msg_send.buf[6] = (motor4 >> 8) & 0xFF; msg_send.buf[7] = motor4 & 0xFF; break; //
     }
-    can3.write(msg_send);
+    can3.write(msg_send); //
 
-    // ID 0x1FF への送信設定 (Motor 5-8)
-    msg_send.id = 0x1FF;
-    msg_send.len = 8;
+    // モーター 5-8 用 (ID 0x1FF)
+    msg_send.id = 0x1FF; //
+    msg_send.len = 8; //
     switch(id_) {
-        case 5:
-            msg_send.buf[0] = (motor5 >> 8) & 0xFF;
-            msg_send.buf[1] = motor5 & 0xFF;
-            break;
-        case 6:
-            msg_send.buf[2] = (motor6 >> 8) & 0xFF;
-            msg_send.buf[3] = motor6 & 0xFF;
-            break;
-        case 7:
-            msg_send.buf[4] = (motor7 >> 8) & 0xFF;
-            msg_send.buf[5] = motor7 & 0xFF;
-            break;
-        case 8:
-            msg_send.buf[6] = (motor8 >> 8) & 0xFF;
-            msg_send.buf[7] = motor8 & 0xFF;
-            break;
+        case 5: msg_send.buf[0] = (motor5 >> 8) & 0xFF; msg_send.buf[1] = motor5 & 0xFF; break; //
+        case 6: msg_send.buf[2] = (motor6 >> 8) & 0xFF; msg_send.buf[3] = motor6 & 0xFF; break; //
+        case 7: msg_send.buf[4] = (motor7 >> 8) & 0xFF; msg_send.buf[5] = motor7 & 0xFF; break; //
+        case 8: msg_send.buf[6] = (motor8 >> 8) & 0xFF; msg_send.buf[7] = motor8 & 0xFF; break; //
     }
-    can3.write(msg_send);
+    can3.write(msg_send); //
 
-    // 受信処理
-    received = can3.read(msg_read);
+    received = can3.read(msg_read); //
     if (received) {
-        uint8_t index = msg_read.id - 0x201;
+        uint8_t index = msg_read.id - 0x201; //
         if (index < 8) {
             for (uint8_t i = 0; i < 3; i++) {
-                int16_t buf = (msg_read.buf[2 * i] << 8) | msg_read.buf[2 * i + 1];
-                status[index][i] = buf;
+                int16_t buf = (msg_read.buf[2 * i] << 8) | msg_read.buf[2 * i + 1]; //
+                status[index][i] = buf; //
             }
-            status[index][3] = msg_read.buf[6]; // 温度/エンコーダ情報
+            status[index][3] = msg_read.buf[6]; //
         }
     }
 }
 
 void RoboMasval::update(int id, int send, int rpm_) {
-    // 入力値を-16384〜16384に制限して各モータ変数に格納
-    int constrained_send = constrain(send, -16384, 16384);
-    
+    int constrained_send = constrain(send, -16384, 16384); //
     switch (id) {
-        case 1: motor1 = constrained_send; break;
-        case 2: motor2 = constrained_send; break;
-        case 3: motor3 = constrained_send; break;
-        case 4: motor4 = constrained_send; break;
-        case 5: motor5 = constrained_send; break;
-        case 6: motor6 = constrained_send; break;
-        case 7: motor7 = constrained_send; break;
-        case 8: motor8 = constrained_send; break;
+        case 1: motor1 = constrained_send; break; //
+        case 2: motor2 = constrained_send; break; //
+        case 3: motor3 = constrained_send; break; //
+        case 4: motor4 = constrained_send; break; //
+        case 5: motor5 = constrained_send; break; //
+        case 6: motor6 = constrained_send; break; //
+        case 7: motor7 = constrained_send; break; //
+        case 8: motor8 = constrained_send; break; //
     }
 }
 
 void RoboMasval::info(uint8_t ID_) {
-    if (ID_ < 1 || ID_ > 8) return;
-    
-    int16_t current_speed = status[ID_ - 1][1];
-    int current_encoder = status[ID_ - 1][3];
-
+    if (ID_ < 1 || ID_ > 8) return; //
+    speed1 = status[ID_ - 1][1]; //
+    // 実際には各変数に代入
     switch(ID_) {
-        case 1: speed1 = current_speed; encoder1 = current_encoder; break;
-        case 2: speed2 = current_speed; encoder2 = current_encoder; break;
-        case 3: speed3 = current_speed; encoder3 = current_encoder; break;
-        case 4: speed4 = current_speed; encoder4 = current_encoder; break;
-        case 5: speed5 = current_speed; encoder5 = current_encoder; break;
-        case 6: speed6 = current_speed; encoder6 = current_encoder; break;
-        case 7: speed7 = current_speed; encoder7 = current_encoder; break;
-        case 8: speed8 = current_speed; encoder8 = current_encoder; break;
+        case 1: speed1 = status[0][1]; encoder1 = status[0][3]; break; //
+        case 2: speed2 = status[1][1]; encoder2 = status[1][3]; break; //
+        case 3: speed3 = status[2][1]; encoder3 = status[2][3]; break; //
+        case 4: speed4 = status[3][1]; encoder4 = status[3][3]; break; //
+        case 5: speed5 = status[4][1]; encoder5 = status[4][3]; break; //
+        case 6: speed6 = status[5][1]; encoder6 = status[5][3]; break; //
+        case 7: speed7 = status[6][1]; encoder7 = status[6][3]; break; //
+        case 8: speed8 = status[7][1]; encoder8 = status[7][3]; break; //
     }
+}
+
+void RoboMasval::set_PID(uint8_t ID_, float kp, float ki, float kd) {
+    // PID設定の実装が必要な場合はここに追加
 }
